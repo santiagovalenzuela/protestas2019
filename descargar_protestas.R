7020d4e320e93812da07ba526959accb79299d30
unknown <valenzuela.bernal.s@gmail.com>
2020-01-20 21:17:16 -0600

    Initial upload

---
+++ descargar_protestas.R
@@ -0,0 +1,42 @@
+rm(list=ls(all=T))
+
+setwd("C:/Users/Santiago/Documents/protestas2019")
+
+library(gdeltr2)
+library(dplyr)
+library(lubridate)
+library(haven)
+
+# D E S C A R G A 
+
+#Generamos el listado de fechas
+fechas <- seq(ymd('2019-01-01'),ymd('2019-12-31'),by='days')
+fechas <- gsub("-","",fechas) #Quitamos los guiones
+
+filenames <- paste0("./downloads/RDS_", fechas,".RData")
+
+descarga <- function(dias){
+  for(dia in 1:(length(dias))){
+    x <- get_data_gdelt_periods_event(period = dias[dia])
+    saveRDS(x, file = filenames[dia])
+    y <-paste0("Guardado", dias[dia])
+    print(y)
+    rm(x)
+    Sys.sleep(25)
+  }
+}
+
+#descarga(fechas)
+
+#Creamos una tibble vacía con los nombres de las columnas y los tipos de variables:
+df <- head(readRDS(file = filenames[1]), n = 0)
+
+# L I M P I E Z A
+for (archivo in filenames){
+  x <-readRDS(file = archivo)
+  x <-x %>% filter(idCAMEOEventRoot == 14) #Nos quedamos unicamente con las protestas
+  df <- bind_rows(df, x)
+  rm(x)
+}
+
+save(df, file = "protestas2019.RData")
